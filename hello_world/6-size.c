f52477ea162ee040035c5fa9faca25ff19b13ef7
Mezanic <ezanic.mael@gmail.com>
2024-06-17 14:17:42 +0200

    Create file 6 for prints the size of various types

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<limits.h>
+/**
+ * main - that prints the size of various types on the computer
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	printf("Size of a char:" % c "byte(s)");
+	printf("Size of an int:" % d "byte(s)");
+	printf("Size of a long int:" % ld "byte(s)");
+	printf("Size of a long long int:" % lu " byte(s)");
+	printf("Size of a float:" % f "byte(s)");
+	return (0);
+}
