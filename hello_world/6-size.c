c91e49432424be2d9f872a29246b0e784cdf45ce
Mezanic <ezanic.mael@gmail.com>
2024-06-17 14:21:40 +0200

    Clear the 2 ", one right after : other after %x on ligne 12,13,14,15,16

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 
-	printf("Size of a char:" %c "byte(s)");
-	printf("Size of an int:" %d "byte(s)");
-	printf("Size of a long int:" %ld "byte(s)");
-	printf("Size of a long long int:" %lu " byte(s)");
-	printf("Size of a float:" %f "byte(s)");
+	printf("Size of a char: %c byte(s)");
+	printf("Size of an int: %d byte(s)");
+	printf("Size of a long int: %ld byte(s)");
+	printf("Size of a long long int: %lu byte(s)");
+	printf("Size of a float: %f byte(s)");
 	return (0);
 }
