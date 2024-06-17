5dd1b73fc4a9ddd3c0a2913f3d80a804a24b6a47
Mezanic <ezanic.mael@gmail.com>
2024-06-17 12:07:39 +0200

    Fix file 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.sh>
 /* Let's start first programm in C */
-int main(void)
-{
+	int main(void)
+	{	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+	}
