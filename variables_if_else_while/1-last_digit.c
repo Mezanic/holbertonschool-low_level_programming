929c2ffe08ec0eb74dc7b9ca7386dbeca938fbad
Mezanic <ezanic.mael@gmail.com>
2024-06-18 09:40:59 +0200

    Remove Main for main in comment, space between fonction and (

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * Main - will assign a random number to n
+ * main - will assign a random number to n
  * and print  print the last digit of the number stored in the variable
  *
  * Return: 0
@@ -17,16 +17,16 @@ int main(void)
 
 	Last_D = n % 10;
 		if (Last_D > 5)
-		{ 
-			printf("Last digit of %d is %d and is greater than 5\n",n,Last_D);
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, Last_D);
 		}
 		else if (Last_D < 6 && Last_D != 0)
 		{
-			printf ("Last digit of %d is %d and is less than 6 and not 0\n",n,Last_D);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_D);
 		}
 		else
 		{
-			printf ("Last digit of %d is %d and is 0",n,Last_D);
+			printf("Last digit of %d is %d and is 0", n, Last_D);
 		}
 
 
