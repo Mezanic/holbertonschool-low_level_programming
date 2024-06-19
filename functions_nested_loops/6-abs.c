1cce96a6f5579fd59f097f16d5bebb95f1b52f80
Mezanic <ezanic.mael@gmail.com>
2024-06-19 15:57:44 +0200

    update main.h for int _abs(int),and create file 6-abs for computes the absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @r: number
+ *
+ * Return: 0
+ */
+
+int _abs(int r)
+{   
+	if (r < 0)
+	{
+		r = r * -1;
+	}
+	return (r);
+}
