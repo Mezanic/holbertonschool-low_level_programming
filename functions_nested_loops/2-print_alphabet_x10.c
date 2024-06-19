9d7b1d1396c7b1e546f78a37f04680894dc74746
Mezanic <ezanic.mael@gmail.com>
2024-06-19 13:33:37 +0200

    Create file 2 for print alphabet 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - will print alphabet 10 time 
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	i = 0;
+
+	while( i < 10 )
+	{
+
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+
+
+		_putchar('\n');
+		i++;
+	}
+	return;
+
+}
