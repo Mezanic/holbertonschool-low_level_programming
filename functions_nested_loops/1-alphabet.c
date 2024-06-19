70a2c317b0baf70fa7d4e21310b2fd257e75343c
Mezanic <ezanic.mael@gmail.com>
2024-06-19 10:17:39 +0200

    Create file 1 for print alphabet, add void print_alphabet(void) in main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - hat prints the alphabet, in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+
+	}
+	_putchar('\n');
+	return (0);
+}
