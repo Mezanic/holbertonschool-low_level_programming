44c228fd7e082ff21644026966e78d77276e3d92
Mezanic <ezanic.mael@gmail.com>
2024-06-21 14:00:54 +0200

    Betty coding style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  * add - addition and return result
- *
+ * @num1: number 1
+ * @num2: number 2
  * Return: result
  */
 
 int add(int num1, int num2)
 {
-	return(num1 + num2);
+	return (num1 + num2);
 }
