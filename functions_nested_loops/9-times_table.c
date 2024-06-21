484a11c532230c870da39161e2456de9bad720aa
Mezanic <ezanic.mael@gmail.com>
2024-06-21 12:34:40 +0200

    Create file 9 for print table time from 0 to 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - print tablec multiplication
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int counter1, counter2, result;
+
+	for (counter1 = 0; counter1 <= 9; counter1++)
+	{
+		_putchar('0');
+
+		for (counter2 = 0; counter2 <= 9; counter2++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = counter1 * counter2;
+
+			if (result < 9)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
