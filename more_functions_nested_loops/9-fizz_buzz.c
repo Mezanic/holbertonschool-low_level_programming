67cc4e81d901a424cd5477e6cae92b4b485a7022
Mezanic <ezanic.mael@gmail.com>
2024-06-24 14:58:27 +0200

    add exlcusion and betty docustyle

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,21 +4,20 @@
 /**
  * fizz buzz - check multiple of 3 and 5
  *
- * Return: void
+ * Return: 0
  */
-
 int main(void)
 {
 	int n;
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3) == 0)
+		if ((n % 3) == 0 && (n % 5) != 0)
 		{
 			printf("Fizz ");
 		}
 
-		else if ((n % 5) == 0)
+		else if ((n % 5) == 0 && (n % 3) != 0)
 		{
 			printf("Buzz ");
 		}
