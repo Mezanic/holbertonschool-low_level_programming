a280bcf685f3f947ca00d1e89e941587e7248d4b
Mezanic <ezanic.mael@gmail.com>
2024-06-24 09:39:07 +0200

    create README, add main.h in more function directory,create file 0 for chech for uppercase character

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+void print_to_98(int n);
+#endif
