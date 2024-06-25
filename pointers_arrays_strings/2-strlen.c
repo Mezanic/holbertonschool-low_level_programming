b71b8efac01d323c57f8b93464739afdd6f0f73b
Mezanic <ezanic.mael@gmail.com>
2024-06-25 10:54:45 +0200

    Update main.h, create file 2 for returns the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: length of string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
