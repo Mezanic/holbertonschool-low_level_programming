b1d10a48affbfc5d36a63d6066d55311597ebb86
Mezanic <ezanic.mael@gmail.com>
2024-06-26 13:31:45 +0200

    update main.h, create file 7 for print only the second part of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ *@str: string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0, j = 0, h = 0;
+
+
+	for  (i = 0; str[i] != 0; i++)
+	{
+	}
+
+	j = i / 2;
+
+	for ( h = j; str[h] != 0; h++)
+	{
+		_putchar(str[h]);
+
+	}
+	_putchar('\n');
+	
+}
