0034736beb94c0be3a700e4f2c8d704acca8e234
Mezanic <ezanic.mael@gmail.com>
2024-06-28 11:31:07 +0200

    update main.h create file 2 for copies string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: string dest
+ * @src: string source
+ * @n: number
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != 0 && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (dest[i] != 0 && i < n)
+	{
+		dest[i] = 0;
+	}
+	return (dest);
+}
