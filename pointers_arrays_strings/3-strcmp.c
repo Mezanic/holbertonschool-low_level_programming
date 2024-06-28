8ab85367d1e8d34e295e18c9988e4b92c7c4101d
Mezanic <ezanic.mael@gmail.com>
2024-06-28 12:30:48 +0200

    update main create file 3 for comapre string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two string
+ * @s1: strinf 1
+ * @s2: string 2
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (s1[i] != 0 || s2[j] != 0)
+	{
+
+		 if (s1[i] != s2[j])
+			return (s1[i] - s2[j]);
+	i++;
+	j++;
+	}
+	return (0);
+}
