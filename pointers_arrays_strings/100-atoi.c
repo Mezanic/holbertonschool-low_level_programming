2c8e2320ed2c9811039de9c59170ae804db32d07
Mezanic <ezanic.mael@gmail.com>
2024-06-29 22:21:36 +0200

    recreate all the code for file 100atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,28 @@
  */
 int _atoi(char *s)
 {
+	int i = 0, n = 1;
+	unsigned int r = 0;
 
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			n = n * -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			r = r * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+		}
+		else if (r > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (n * r);
 }
