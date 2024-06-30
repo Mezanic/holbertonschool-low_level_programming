16ccdf84790d63383c2b93f7909889b820c20ba4
Mezanic <ezanic.mael@gmail.com>
2024-06-30 17:16:03 +0200

    refontee the code

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string -  capitalizes all words of a string.
- * @uwu: char 
+ * @uwu: char
  *
  * Return: Char
  */
@@ -10,28 +10,30 @@
 char *cap_string(char *uwu)
 {
 	int i = 0, j = 0;
-	char arr[13] = " !?(){},;.\"\t\n";
-	
+	char arr[] = " !?(){},;.\"\t\n";
+
 
 	for  (i = 0; uwu[i] != '\0'; i++)
 	{
-		if (i == 0 )
-		{	
+		if (i == 0)
+		{
 			if (uwu[i] >= 'a' && uwu[i] <= 'z')
 			{
 				uwu[i] = uwu[i] - 32;
 			}
-				for (j = 0; j < 14; j++)
+		}
+		else
+		{
+			for (j = 0; arr[j]; j++)
+			{
+				if (uwu[i] == arr[j])
 				{
-					if (uwu[i] == arr[j])
+					if (uwu[i + 1] >= 'a' && uwu[i + 1] <= 'z')
 					{
-						if (uwu[i + 1] >= 'a' && uwu[i + 1] <= 'z')
-						{
-							uwu[i + 1] = uwu[i + 1] - 32;
-						}
+						uwu[i + 1] = uwu[i + 1] - 32;
 					}
 				}
-
+			}
 		}
 	}
 	return (uwu);
