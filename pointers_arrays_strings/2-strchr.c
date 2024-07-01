57d84e5cfda762646b14511ce261062152fe1994
Mezanic <ezanic.mael@gmail.com>
2024-07-01 14:26:17 +0200

    code overhaul

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,17 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != c && s[i] != '\0')
+	while (s[i] != '\0')
 	{
+		if(s[i] == c)
+		{
+			return (s +i);
+		}
+		else if (c == '\0')
+		{
+			return (s +i);
+		}
 		i++;
 	}
-		if (c != '\0')
-		{
-			return (s + i);
-		}
 	return (s + i);
 }
