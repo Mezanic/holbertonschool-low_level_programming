8d1536923125ad04342c18b7c6e0a039659a614f
Mezanic <ezanic.mael@gmail.com>
2024-07-01 16:17:49 +0200

    code overhaul

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,12 +10,16 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0, j = 0;
+	int result = 0;
 
-	while (s[i] != 0)
+	while (s[i] != 0 || accept[j] != 0)
+	{
+		if (accept[j] == s[i])
+		{
+			result += 1;
+			j++;
+		}
 		i++;
-
-	while (accept[j] != 0)
-		j++;
-
-	return ((i + 1) - (j + 1));
+	}
+	return (result + 1);
 }
