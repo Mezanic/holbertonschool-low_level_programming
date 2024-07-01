57bf48f025571110f82c11a762b032b8c4f4428b
Mezanic <ezanic.mael@gmail.com>
2024-07-01 15:27:17 +0200

    change return logic

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: initial segment
- * @accept: bytes we need 
+ * @accept: bytes we need
  *
  * Return: accept
  */
@@ -11,11 +11,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0, j = 0;
 
-	while ( s[i] != 0)
+	while (s[i] != 0)
 		i++;
 
 	while (accept[j] != 0)
 		j++;
 
-	return ( j + 1);	
+	return ((i + 1) - (j + 1));
 }
