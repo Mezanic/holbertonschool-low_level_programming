7f8000a96e222c78aa047efb9f5bb999796fde71
Mezanic <ezanic.mael@gmail.com>
2024-07-01 17:49:09 +0200

    add if for break if result bigger than j

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,14 +12,18 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, j = 0;
 	int result = 0;
 
-	while (s[i] != 0 || accept[j] != 0)
+	while (s[i] != 0)
 	{
-		if (accept[j] == s[i])
+		for (j = 0; accept[j] != 0; j++)
 		{
-			result += 1;
-			j++;
+			if (accept[j] == s[i])
+			{
+				result += 1;
+			}
 		}
+		if (result > j)
+			break;
 		i++;
 	}
-	return (result + 1);
+	return (result);
 }
