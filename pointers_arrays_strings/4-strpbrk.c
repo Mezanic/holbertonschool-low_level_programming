f66b51e36882f162e608cbee1a60edad84678dc0
Mezanic <ezanic.mael@gmail.com>
2024-07-01 19:38:44 +0200

    change for in while for try

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
 				return (s + i);
 			}
+			j++;
 		}
+		i++;
 	}
 	return (0);
 }
