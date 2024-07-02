d172145d03890ed6ba1ac01ebc1791d38d6db1df
Mezanic <ezanic.mael@gmail.com>
2024-07-02 10:28:31 +0200

    code overhaul, for logic

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strstr - locates a substring
  * @haystack: primodial string
@@ -11,18 +11,24 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0;
 
+	if (needle[j] == '\0')
+	{ 
+		return (haystack);
+	}
 	for  (i = 0; haystack[i] != '\0'; i++)
 	{
-
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[0] && haystack[i + j] == needle[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (haystack + i);
+				break;
 
 			}
 		}
+	if (needle[j] == '\0')
+	{
+		return (&haystack[i]);
 	}
-
+	}
 	return (0);
 }
