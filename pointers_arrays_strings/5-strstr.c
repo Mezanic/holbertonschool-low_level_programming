bacd7aae15bd7bc5f19282d9e7d22f78391a424b
Mezanic <ezanic.mael@gmail.com>
2024-07-02 10:15:43 +0200

    code overhaul

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,22 +13,15 @@ char *_strstr(char *haystack, char *needle)
 
 	for  (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-
-			for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i] == needle[0] && haystack[i + j] == needle[j])
 			{
-				if (haystack[i + j] != 0 && haystack [i +j] == needle[j])
-				{ 
-					return (haystack + i);
+				return (haystack + i);
 
-				}	
 			}
 		}
-	}
-	if (needle[j] == '\0')
-	{
-		return (haystack + i);
 	}
 
 	return (0);
