e43422390b7bffdc1f22d52a978d9faa0630299e
Mezanic <ezanic.mael@gmail.com>
2024-07-02 09:36:20 +0200

    thanks mr Callejon

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _strstr - locates a substring
  * @haystack: primodial string
@@ -11,17 +11,23 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0;
 
-	while (haystack[i] != 0)
+	for  (i = 0; haystack[i] != '\0'; i++)
 	{
-		while (needle[j] != 0)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (needle[0] == haystack[i])
 			{
-				return (haystack + i);
+				if (haystack[i] == needle[j]  )   
+				{
+					return (haystack +i);
+
+				}
 			}
-			j++;
 		}
-		i++;
+	}
+	if (needle[j] == '\0')
+	{
+		return (haystack + i);
 	}
 
 	return (0);
