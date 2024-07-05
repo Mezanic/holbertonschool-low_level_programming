fda42ee9e62e9993fd91fccadf545646d64fd6b8
Mezanic <ezanic.mael@gmail.com>
2024-07-05 13:31:03 +0200

    Create file 5 for find the square root, and update main with the function inside file 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return natural square root
+ * @n: number we nned to return natural square
+ *
+ * Return: square of a number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_r(n, 0));
+	}
+}
+
+/**
+ * _sqrt - return natural square root
+ * @n: we number ned to return natural square
+ * @i: find the square
+ *
+ * Return: i or -1
+ */
+
+int _sqrt_r(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_r(n, i + 1));
+	}
+}
