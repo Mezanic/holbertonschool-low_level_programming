3ab5eab3b9c315b7870efdb222c3c8c560fec519
Mezanic <ezanic.mael@gmail.com>
2024-07-08 11:34:08 +0200

    Add README, main for local library, file 0 for create an array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - creates array of chars initializes with specific char
+ * @size: size of array
+ * @c: char
+ *
+ * Return: a pointer to array or 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
