b9197c5d4f8213fb154ce6b2065a18364f48a3ab
Mezanic <ezanic.mael@gmail.com>
2024-07-08 11:54:44 +0200

    remove and recreate all

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,13 +15,15 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	if (size == 0)
+	{
 		return (NULL);
-
+	}
 	arr = malloc(sizeof(char) * size);
 
 	if (arr == 0)
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -42,5 +42,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 #endif
