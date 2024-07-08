522391498617736f97d56a7f5bd4cc0e8ea416b8
Mezanic <ezanic.mael@gmail.com>
2024-07-08 14:37:05 +0200

    update main for add function for file 2, create file 2 for concatenates two string in a new memory space

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat -  concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 or s1 followed by s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, h = 0;
+	char *concatenates;
+
+	if (s1 == 0 || s2 == 0)
+		return (NULL);
+
+	while (s1[i] != 0)
+		i++;
+
+	while (s2[j] != 0)
+		j++;
+
+	concatenates = malloc(sizeof(char) * (i + j) + 1);
+
+		if (concatenates == 0)
+			return (NULL);
+	for (h = 0; h < i; h++)
+	{
+		concatenates[h] = s1[h];
+	}
+
+	for (j = 0; s2[j] != 0; j++, h++)
+	{
+		concatenates[h] = s2[j];
+	}
+	return (concatenates);
+}
