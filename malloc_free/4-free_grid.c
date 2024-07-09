3a009d1fe194f3a85b3370c40575f209e0897484
Mezanic <ezanic.mael@gmail.com>
2024-07-09 15:10:22 +0200

    update main for file 4 and create file 4 for free file 3

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid -  frees a 2 dimensional 
+ * @**grid: 
+ * @height: hauteur 
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
