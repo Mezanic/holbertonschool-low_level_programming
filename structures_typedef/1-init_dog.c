ab07311bec8eb64f053710238d8ba2e62f6260a4
Mezanic <ezanic.mael@gmail.com>
2024-07-11 14:02:03 +0200

    betty style

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,19 +4,19 @@
 /**
  * init_dog - initialize variable stuct dog
  * @d: pointer
- * @name: name of dog 
- * @owner: owner of dog 
+ * @name: name of dog
+ * @owner: owner of dog
  * @age: age of dog
  *
  * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
+{
 	if (d == NULL)
 		return;
-	d-> name = name;
-	d-> owner = owner;
-	d-> age = age;
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
 
 }
