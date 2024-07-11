186da7eef92189fc189689a91b818bfced46d480
Mezanic <ezanic.mael@gmail.com>
2024-07-11 14:58:15 +0200

    change the code logic

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -29,7 +29,10 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 		return;
 	}
+	else
+	{
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
+	}
 }
