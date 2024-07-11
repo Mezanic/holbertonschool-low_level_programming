d6b8c9eeac4a021ad82424f49d68720b0e9399ac
Mezanic <ezanic.mael@gmail.com>
2024-07-11 14:36:27 +0200

    betty style and readd condition for name == NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,9 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL || d->age == 0)
 		printf("(nil)\n");
-
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
