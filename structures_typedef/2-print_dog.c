74257c17fb73fe8ba997e613c7ad24ed8bbbe1cf
Mezanic <ezanic.mael@gmail.com>
2024-07-11 14:42:00 +0200

    remove || operator, add if for age, betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,11 +12,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->owner == NULL || d->age == 0)
+	if (d->owner == NULL)
 		printf("(nil)\n");
+
+	if (d->age == 0)
+		printf("(nil)\n");
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	else 
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
