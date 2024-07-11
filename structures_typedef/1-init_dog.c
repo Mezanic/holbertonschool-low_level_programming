f0a85798339e9c7a01bdcbe7b5476562e5930c38
Mezanic <ezanic.mael@gmail.com>
2024-07-11 13:59:10 +0200

    create file 1 for initialyse variable of type struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize variable stuct dog
+ * @d: pointer
+ * @name: name of dog 
+ * @owner: owner of dog 
+ * @age: age of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{	
+	if (d == NULL)
+		return;
+	d-> name = name;
+	d-> owner = owner;
+	d-> age = age;
+
+}
