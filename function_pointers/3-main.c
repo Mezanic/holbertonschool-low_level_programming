babcdd0285fc9029fb93310a794cad35764fe1cf
Mezanic <ezanic.mael@gmail.com>
2024-07-16 14:08:53 +0200

    change logic

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,15 +21,15 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-
 	if (!op)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
 	if ((*op == '/' || *op == '%') && b == 0)
 	{
