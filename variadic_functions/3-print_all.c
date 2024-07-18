2027a8a258be1007d7850ea0294fe44e54c80211
Mezanic <ezanic.mael@gmail.com>
2024-07-18 15:20:00 +0200

    update variadic_functions.h with the proto for function in file 3, Create file 3 for print anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of all arguments
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *string;
+
+	va_start(args, format);
+
+	while (format[i])
+	{	
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+
+			case 'i':
+				printf("%i", va_arg(args, int));
+				break;
+
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+
+			case 's':
+				string = va_arg(args, char *);
+
+				if (string == NULL)
+					string = "(nil)";
+
+				printf("%s", string);
+				break;
+		}
+		if (format[i + 1] && format[i - 1] != 0)
+			printf(", ");
+
+		i++;
+		va_end(args);
+	}
+	printf("\n");
+}
