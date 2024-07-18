24b692098ac8ff983ad48e94ea1c29f273dcbee5
Mezanic <ezanic.mael@gmail.com>
2024-07-18 14:21:22 +0200

    change the logic behind if condition for string == NULL

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(args, char *);
 
 		if (string == NULL)
-			printf("(nil)");
+			string = "(nil)";
 
 		printf("%s", string);
 
