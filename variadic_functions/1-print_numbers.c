a43e05c715f1db8b27daa288e050843803f464a2
Mezanic <ezanic.mael@gmail.com>
2024-07-18 13:37:28 +0200

    Update variadic_fucntions.h for proto of function use in file 1, create file 1 for print number

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  print number 
+ * @separator: string to be printed
+ * @n: number
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int num = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+
+		if (separator == NULL || i == (n - 1))
+			printf("%d",num);
+
+		else
+		printf("%i%s",num ,separator);
+
+	}
+	printf("\n");
+	va_end(args);
+
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef VF_H
 #define VF_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
