3eee224fd5d187620b9d0ca592caf57e09a063dc
Mezanic <ezanic.mael@gmail.com>
2024-07-30 05:56:17 +0200

    change the logic of the code

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,10 +15,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
+/* set up new_node with value n and for became end of the list */
+	new_node->next = NULL;
+	new_node->n = n;
 
+	/**
+	 * check if the list is empty and
+	 * make new_node head
+	 */
 	if (*head == NULL)
 	{
-		new_node->next = NULL;
 		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
@@ -29,9 +35,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (last->next != NULL)
 		last = last->next;
 
-	new_node->next = NULL;
-	new_node->prev = last;
-	new_node->n = n;
 	last->next = new_node;
+	new_node->prev = last;
 	return (new_node);
 }
