72545471bfbf5fea4254b32b317243e1024d21fd
Mezanic <ezanic.mael@gmail.com>
2024-07-30 07:26:56 +0200

    Added file 6 for do the sum of all element in the list, update lists.h with the prototype of the function use in file 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint : function that return sum of all date in a linked list
+ * @head: poitner to the head of the double linked list
+ *
+ * Return: sum of all data or 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+
+		sum += head->n;
+		head = head->next;
+
+	}
+
+	return (sum);
+}
