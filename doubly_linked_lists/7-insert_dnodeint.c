1e887e6d95f58dfbc6c8412b0f65e26a1b0ccbb4
Mezanic <ezanic.mael@gmail.com>
2024-07-31 14:43:16 +0200

    Added file 7 for insert a node in double linked list, update lists.h with the prtoype of the file 7 function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,27 +11,38 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	unsigned len = dlistint_len(head);
-	dlistint_t *curr = *head;
+	dlistint_t *curr = *h;
+	dlistint_t *new_node;
 
-	if (idx < 1)
+	if (h == NULL)
 		return (NULL);
 
-	if (idx == 1)
+	/* insert new node at the beginning of the list*/
+	if (idx == 0)
 		return (add_dnodeint(h, n));
-		
-	if (idx == len)
+
+	/* insert it everywere except beginning or end */
+	for (i = 0; curr != NULL && i < idx - 1; i++)
+		curr = curr->next;
+
+	/* insert new node at the end of the list */
+	if (curr->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	
-	for (i = 1; curr != NULL && i < idx - 1;i++)
-	curr = curr->next;
-	
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->next = curr->next;
 	new_node->prev = curr;
-	curr->prev = new_node
+	curr->next = new_node;
+
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+
+	return (new_node);
 
 
 
