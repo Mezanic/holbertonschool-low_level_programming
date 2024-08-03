cecaee12bcd20a2fae09b1bb3a027a2c6361d3be
Mezanic <ezanic.mael@gmail.com>
2024-08-03 22:09:06 +0200

    Added file 2 for get value of a bit at x index, update main.h witht he protoype function for file 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @index:  the index, starting from 0 of the bit you want to get
+ * @n: integer
+ *
+ * Return: value of the bit or -1 
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	n >>= index;
+
+	return (n & 1);
+}
