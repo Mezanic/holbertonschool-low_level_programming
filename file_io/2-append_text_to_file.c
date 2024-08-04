42da2a3dd954f7146ffe6d4d4682c5903d1f60b2
Mezanic <ezanic.mael@gmail.com>
2024-08-04 21:35:20 +0200

    Added file 2 for wirte at the end of the file, update main.h with the function prototype for file 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: text need to be printed
+ *
+ * Return: 1 or -1 if failed
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_des;
+	int text_len = 0, file_w;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	while (text_content[text_len])
+		text_len++;
+
+	file_des = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_des == -1)
+		return (-1);
+
+	file_w = write(file_des, text_content, text_len);
+
+	if (file_w == -1)
+		return (-1);
+
+	close(file_des);
+	return (1);
+}
