36947dd9cc2957a0cf2da66fbf49618ab055c9dc
Mezanic <ezanic.mael@gmail.com>
2024-08-04 21:03:40 +0200

    initialize int

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_des;
-	ssize_t file_w, text_len;
+	ssize_t file_w = 0, text_len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -33,6 +33,4 @@ int create_file(const char *filename, char *text_content)
 
 	close(file_des);
 	return (1);
-
-
 }
