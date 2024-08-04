1133379ad9bc8498f5feb486e9d53f911b7afb91
Mezanic <ezanic.mael@gmail.com>
2024-08-04 21:39:46 +0200

    remove nouse line

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	file_des = open(filename, O_WRONLY | O_APPEND);
 
-	if (file_int append_text_to_file(const char *filename, char *text_content);des == -1)
+	if (file_des == -1)
 		return (-1);
 
 	file_w = write(file_des, text_content, text_len);
