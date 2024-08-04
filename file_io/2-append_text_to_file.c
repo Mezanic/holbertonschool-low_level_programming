056021f606eeef1375c696cc818638ef33b8f122
Mezanic <ezanic.mael@gmail.com>
2024-08-04 21:59:31 +0200

    Betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	else
-       	{
+	{
 
 		while (text_content[text_len])
 			text_len++;
