7f0764531574e9c408d65475b4a62eba8b9d91e2
Mezanic <ezanic.mael@gmail.com>
2024-08-04 20:54:48 +0200

    Added file 1 for create a file, update main.h with the function prototype use in function 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ *
+ * Return: 1 or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_des;
+	ssize_t file_w, text_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[text_len])
+		text_len++;
+
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if(file_des == -1)
+		return (-1);
+
+	file_w = write(file_des, text_content, text_len);
+
+	if (file_w == -1)
+		return (-1);
+
+	close(file_des);
+	return (1);
+
+
+}
