67ea5fe59ccf9d07ba9b52664954b5116ddf5431
Mezanic <ezanic.mael@gmail.com>
2024-08-04 19:44:13 +0200

    Added README for new repo, added main.h for became my header file in this project, added file 0 for  function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text file prints it to the POSIX standard output.
+ * @filename: file need to be read
+ * @letters: nomber of char in file
+ *
+ * Return: actual number of letter or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_des;
+	char *file_buffer;
+	ssize_t bytes_count, file_r, file_w;
+
+	if (filename == NULL)
+		return (0);
+
+	file_des = open(filename, O_RDONLY)
+		if (file_des == -1)
+			return (0);
+
+
+
+	file_buffer = malloc(sizeof(char) * letters);
+	if (file_buffer == NULL)
+	{
+		close(file);
+		return (0);
+
+	}
+
+	file_r = read(files_des, file_buffer, letters);
+	if (r == -1)
+	{
+		close(file_des);
+		return (0);
+	}
+
+	bytes_count = write(STDOUT_FILENO, file_buffer, file_r);
+
+	close(file_des);
+	free(buffer);
+	return (bytes_count);
+
+}
